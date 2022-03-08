6daa4a16ae9dc11c3777336aacb67389c4753ac5
guisantosalves <workguisantos@gmail.com>
2022-03-08 15:29:02 -0400

    numeros_primos_implementados

--- loops/numeros_primos.py
+++ loops/numeros_primos.py
@@ -1,6 +1,17 @@
-# num = int(input("Digite um numero: "))
+count = 2
+resultt = 0
 
-# if SymPy.isprime(num):
-#     print("É primo")
-# else:
-#     print("nao é")
+num = int(input("Digite um número: "))
+
+while count <= (num/2):
+    if num % count == 0:
+        resultt = resultt + 1
+        break;
+    else:
+        break
+count = count + 1 
+    
+if resultt == 0:
+    print("é um npumero primo ")
+else:
+    print("Não é um número primo")
